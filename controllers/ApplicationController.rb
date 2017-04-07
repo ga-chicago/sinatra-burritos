e60a8f442adbca9f4225ecca26fd297313d1c6b9
jeffkrantz <jjkrantz@gmail.com>
2017-04-07 09:43:55 -0500

    db

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -4,8 +4,7 @@ class ApplicationController < Sinatra::Base
   Bundler.require
 
   ActiveRecord::Base.establish_connection(
-    :adapter => ENV['DATABASE_URL'] || 'postgresql',
-    :database => 'mexican_food'
+    :adapter => postgres://swavvsfoekkzex:b51c122e4763bdfd9c1eb0a77f27a73f98d7a57ff6cbed2cbe5721783dd38efa@ec2-23-21-246-11.compute-1.amazonaws.com:5432/d9d6k1ulpdfq2u
   )
 
   set :views, File.expand_path('../../views', __FILE__)
